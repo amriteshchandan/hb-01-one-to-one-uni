bd7de12251fe7399b2e1b2a76898446d7e9477fb
amriteshchandan <amriteshchandan@yahoo.in>
2020-03-15 16:52:00 +0530

    @OneToOne Mapping Done

--- src/com/amritesh/hibernate/demo/CreateDemo.java
+++ src/com/amritesh/hibernate/demo/CreateDemo.java
@@ -4,22 +4,22 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.amritesh.hibernate.entity.demo.Student;
+import com.amritesh.hibernate.entity.demo.Instructor;
+import com.amritesh.hibernate.entity.demo.InstructorDetail;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
 	public static void main(String[] args) {
 		SessionFactory sessionFactory = new Configuration()
 										.configure("hibernate.cfg.xml")
-										.addAnnotatedClass(Student.class)
+										.addAnnotatedClass(Instructor.class)
+										.addAnnotatedClass(InstructorDetail.class)
 										.buildSessionFactory();
 		
-		Session session = sessionFactory.getCurrentSession();
+		Session session = null;
 		
 		try {
-			Student student1 = new Student("Amritesh", "Chandan", "amritesh.cse@gmail.com");
 			session.beginTransaction();
-			session.save(student1);
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/com/amritesh/hibernate/entity/demo/Instructor.java
+++ src/com/amritesh/hibernate/entity/demo/Instructor.java
@@ -1,26 +1,19 @@
 package com.amritesh.hibernate.entity.demo;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.GeneratorType;
-
 @Entity
-@Table(name = "student")
-public class Student {
+@Table(name = "instructor")
+public class Instructor {
 
-	public Student() {}
-	
-	public Student(String firstName, String lastName, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
@@ -35,9 +28,17 @@ public class Student {
 	@Column(name = "email")
 	private String email;
 
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
+	private InstructorDetail instructorDetail;
+	
+	public Instructor() {}
+
+	public Instructor(String firstName, String lastName, String email, InstructorDetail instructorDetail) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.instructorDetail = instructorDetail;
 	}
 
 	public int getId() {
@@ -71,4 +72,19 @@ public class Student {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public InstructorDetail getInstructorDetail() {
+		return instructorDetail;
+	}
+
+	public void setInstructorDetail(InstructorDetail instructorDetail) {
+		this.instructorDetail = instructorDetail;
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+	
 }
