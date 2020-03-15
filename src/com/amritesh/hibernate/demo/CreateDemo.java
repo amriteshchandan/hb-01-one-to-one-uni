5a8a50b3dc46046b06b8b064c754efe0f9cebe9d
amriteshchandan <amriteshchandan@yahoo.in>
2020-03-15 20:40:16 +0530

    Savings @OneToOne Mapped Objects To DB

--- src/com/amritesh/hibernate/demo/CreateDemo.java
+++ src/com/amritesh/hibernate/demo/CreateDemo.java
@@ -19,7 +19,15 @@ public class CreateDemo {
 		Session session = null;
 		
 		try {
+//			Instructor instructor1 = new Instructor("Amritesh", "Chandan", "amritesh.cse@gmail.com");
+//			InstructorDetail instructorDetail1 = new InstructorDetail("https://youtube.com/amritesh", "Love 2 Code !!!");
+//			instructor1.setInstructorDetail(instructorDetail1);
+			session = sessionFactory.getCurrentSession();
 			session.beginTransaction();
+			InstructorDetail instructorDetail1 = session.get(InstructorDetail.class, 3);
+			Instructor instructor1 = new Instructor("Ankur", "B", "ankur.b@gmail.com");
+			instructor1.setInstructorDetail(instructorDetail1);
+			session.save(instructor1);
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
